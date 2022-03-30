dea96c25e1ce8aa29225d6732c6365f9595c7473
Kim Björkman <bjorkman.art@gmail.com>
2022-03-30 09:49:44 +0200

    why is it not working...

--- src/components/dom/search/index.jsx
+++ src/components/dom/search/index.jsx
@@ -18,7 +18,7 @@ export const Search = ({ disabled, ...props }) => {
         aria-disabled={disabled}
         type="submit"
         style="solid"
-        css={{ height: '100%' }}
+        css={{ height: '100%', flex: '1 1 auto', '@sm': { flex: '1 0 auto' } }}
       >
         Submit
       </Button>
