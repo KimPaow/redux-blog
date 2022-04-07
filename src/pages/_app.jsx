34b93690667a5511889c5f42563e51e5ee38bd73
Kim Björkman <bjorkman.art@gmail.com>
2022-04-07 20:37:38 +0200

    add fathom

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,8 @@
 
 import { Provider } from 'react-redux';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 import ErrorBoundary from '@/components/dom/error-boundary';
 import { darkTheme, lightTheme } from '@/theme/stitches.config';
 import { globalStyles } from "@/theme/globalStyles"
@@ -11,6 +13,25 @@ import store from '@/store';
 function MyApp({ Component, pageProps }) {
   const [dark, setDark] = useState(false)
   const prefersDarkMode = usePrefersDarkMode();
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load('OJSCTAOQ', {
+      includedDomains: ['reduxblog.bjorkman.kim', 'wwww.reduxblog.bjorkman.kim', 'bjorkman.kim', 'www.bjorkman.kim'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     setDark(prefersDarkMode)
